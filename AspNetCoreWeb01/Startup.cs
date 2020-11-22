84b48412427ba7451752f25515df8d125db162e3
farb <farbguo@qq.com>
2020-11-23 00:47:25 +0800

    自定义中间件练习

--- AspNetCoreWeb01/Startup.cs
+++ AspNetCoreWeb01/Startup.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -28,7 +30,7 @@ namespace AspNetCoreWeb01
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
@@ -38,10 +40,27 @@ namespace AspNetCoreWeb01
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+            app.UseStatusCodePages();
             app.UseStaticFiles();
 
             app.UseRouting();
+            #region 中间件的使用练习
 
+            //这是一个中断式中间件，会停止后面的管道处理流程
+            //app.Run(ctx => ctx.Response.WriteAsync("this is a terminal middleware" + Environment.NewLine));
+
+            //app.Use(next =>
+            //{
+            //    Console.WriteLine("这是第1个中间件！");
+            //    return new RequestDelegate(async (context) =>
+            //    {
+            //        await next(context);
+            //        await context.Response.WriteAsync("middleware start..."+Environment.NewLine);
+            //        await context.Response.WriteAsync("middleware end." + Environment.NewLine);
+            //    });
+            //});
+
+            #endregion
             app.UseAuthorization();
             app.UseSession();
             loggerFactory.AddLog4Net();//添加Log4net中间件
