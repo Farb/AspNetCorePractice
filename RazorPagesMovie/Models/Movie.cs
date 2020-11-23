34cdf7d2530598be5ba41313264b0f8118aef718
farb <farbguo@qq.com>
2020-11-23 23:42:04 +0800

    Razor Pages入门教程练习完成

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,16 +1,31 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+        [StringLength(maximumLength:60,MinimumLength =3)]
+        [Required]
         public string Title { get; set; }
 
+        [Display(Name ="Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName ="decimal(18,2)")]
         public decimal Price { get; set; }
+        [Required]
+        [StringLength(5)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        public string Rating { get; set; }
     }
 }
